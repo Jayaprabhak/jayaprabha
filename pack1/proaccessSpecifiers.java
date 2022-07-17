c7f00fac972e01c0cc187e70af05bfa76a5725c7
Jayaprabha <prabhakarthikyn.gmail.com>
2022-07-17 20:09:41 +0530

    changes made

---
+++ pack1/proaccessSpecifiers.java
@@ -0,0 +1,7 @@
+package pack1;
+
+public class proaccessSpecifiers {
+protected void display() {
+	System.out.println("You are using protected access specifiers");
+}
+}
---
+++ pack1/pubaccessSpecifier.java
@@ -0,0 +1,7 @@
+package pack1;
+
+public class pubaccessSpecifier {
+public void display() {
+	System.out.println("You are using public access specifier");
+}
+}
