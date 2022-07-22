1335b4436821bffeffb77a0e6f4bd8201d3f6bf6
Jayaprabha <prabhakarthikyn.gmail.com>
2022-07-22 18:23:28 +0530

    done

---
+++ Assingment/Phase 1/Lesson 3/demo.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class demo 
+{
+    public static void main(String args[]) 
+    {
+        int[] array = new int[3];
+        try 
+        {
+            array[7] = 3;
+        }
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Array index is out of bounds!"); 
+        }
+        finally 
+        {
+            System.out.println("The array size is " + array.length);
+        }
+    }
+}
