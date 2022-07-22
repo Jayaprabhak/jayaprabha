b161d4d3665b579ac3b997b279224254f0d2adf8
Jayaprabha <prabhakarthikyn.gmail.com>
2022-07-22 12:49:07 +0530

    done

--- pack3/accessSpecifier3.java
+++
@@ -1,9 +0,0 @@
-package pack3;
-
-import pack1.*;
-public class accessSpecifier3 extends proaccessSpecifiers{
-public static void main(String[] args) {
-	accessSpecifier3 obj = new accessSpecifier3();
-	obj.display();
-}
-}
