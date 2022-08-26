d96f48760c73ae3696250be4cb288f3302f0856c
Jayaprabha <prabhakarthikyn.gmail.com>
2022-08-26 22:56:28 +0530

    done

---
+++ UserAuthentication/src/main/java/com/simplilearn/demo/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class UserAuthentication {
+	public String username()
+	{
+		String email = "prabha@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "1234";
+		return password;
+	}
+	
+}
